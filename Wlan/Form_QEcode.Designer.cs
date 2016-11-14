90740db4342ee14544118aad2560c8d8d611b7f5
ShanatoYuuji <c1273099082@qq.com>
2016-11-14 13:41:39 +0900

    二维码功能修正

--- Wlan/Form_QEcode.Designer.cs
+++ Wlan/Form_QEcode.Designer.cs
@@ -31,21 +31,24 @@
             this.btn_QEcode = new System.Windows.Forms.Button();
             this.txt_in = new System.Windows.Forms.TextBox();
             this.lab_txt = new System.Windows.Forms.Label();
+            this.btn_show = new System.Windows.Forms.Button();
+            this.PicB_show = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PicB_show)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_QEcode
             // 
-            this.btn_QEcode.Location = new System.Drawing.Point(272, 211);
+            this.btn_QEcode.Location = new System.Drawing.Point(398, 158);
             this.btn_QEcode.Name = "btn_QEcode";
             this.btn_QEcode.Size = new System.Drawing.Size(75, 23);
             this.btn_QEcode.TabIndex = 0;
-            this.btn_QEcode.Text = "二维码";
+            this.btn_QEcode.Text = "生成二维码";
             this.btn_QEcode.UseVisualStyleBackColor = true;
             this.btn_QEcode.Click += new System.EventHandler(this.btn_QEcode_Click);
             // 
             // txt_in
             // 
-            this.txt_in.Location = new System.Drawing.Point(196, 119);
+            this.txt_in.Location = new System.Drawing.Point(398, 102);
             this.txt_in.Name = "txt_in";
             this.txt_in.Size = new System.Drawing.Size(261, 21);
             this.txt_in.TabIndex = 1;
@@ -53,22 +56,43 @@
             // lab_txt
             // 
             this.lab_txt.AutoSize = true;
-            this.lab_txt.Location = new System.Drawing.Point(35, 122);
+            this.lab_txt.Location = new System.Drawing.Point(396, 38);
             this.lab_txt.Name = "lab_txt";
             this.lab_txt.Size = new System.Drawing.Size(113, 12);
             this.lab_txt.TabIndex = 2;
             this.lab_txt.Text = "请输入你要的语句：";
             // 
+            // btn_show
+            // 
+            this.btn_show.Location = new System.Drawing.Point(398, 238);
+            this.btn_show.Name = "btn_show";
+            this.btn_show.Size = new System.Drawing.Size(75, 23);
+            this.btn_show.TabIndex = 3;
+            this.btn_show.Text = "显示二维码";
+            this.btn_show.UseVisualStyleBackColor = true;
+            this.btn_show.Click += new System.EventHandler(this.btn_show_Click);
+            // 
+            // PicB_show
+            // 
+            this.PicB_show.Location = new System.Drawing.Point(77, 24);
+            this.PicB_show.Name = "PicB_show";
+            this.PicB_show.Size = new System.Drawing.Size(250, 250);
+            this.PicB_show.TabIndex = 4;
+            this.PicB_show.TabStop = false;
+            // 
             // Form_QEcode
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(492, 323);
+            this.ClientSize = new System.Drawing.Size(756, 338);
+            this.Controls.Add(this.PicB_show);
+            this.Controls.Add(this.btn_show);
             this.Controls.Add(this.lab_txt);
             this.Controls.Add(this.txt_in);
             this.Controls.Add(this.btn_QEcode);
             this.Name = "Form_QEcode";
             this.Text = "Form_QEcode";
+            ((System.ComponentModel.ISupportInitialize)(this.PicB_show)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,5 +103,7 @@
         private System.Windows.Forms.Button btn_QEcode;
         private System.Windows.Forms.TextBox txt_in;
         private System.Windows.Forms.Label lab_txt;
+        private System.Windows.Forms.Button btn_show;
+        private System.Windows.Forms.PictureBox PicB_show;
     }
 }
--- Wlan/Form_QEcode.cs
+++ Wlan/Form_QEcode.cs
@@ -20,11 +20,11 @@ namespace Wlan
         {
             InitializeComponent();
         }
-
+        private int m = 0;
         private void btn_QEcode_Click(object sender, EventArgs e)
         {
             String text = txt_in.Text.Trim();
-           
+
             BarcodeWriter writer = new BarcodeWriter();
             writer.Format = BarcodeFormat.QR_CODE;//设置生成的图片的样式
             QrCodeEncodingOptions options = new QrCodeEncodingOptions();
@@ -32,16 +32,34 @@ namespace Wlan
 
             options.CharacterSet = "UTF-8";//内容编码
 
-            options.Width = 350;//宽度
-            options.Height = 350;//高度
-
+            options.Width = 250;//宽度
+            options.Height = 250;//高度
             options.Margin = 1;//边距
             writer.Options = options;
 
-            Bitmap map = writer.Write(text);
-            string filename = @"D:\generate1.png";
-            map.Save(filename, ImageFormat.Png);
-            map.Dispose();
+            if (text != null && text != "")
+            {
+                Bitmap map = writer.Write(text);
+                string filename = @"D:\generate1.png";
+               
+                map.Save(filename, ImageFormat.Png);
+                map.Dispose();
+
+            }
+        }
+        private void btn_show_Click(object sender, EventArgs e)
+        {
+            string pngaddress = @"D:\generate1.png";
+            Bitmap bmp = new Bitmap(pngaddress);
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            Graphics draw = Graphics.FromImage(bmp2);
+            draw.DrawImage(bmp, 0, 0);
+
+            this.PicB_show.Image = bmp2;
+            draw.Dispose();
+            bmp.Dispose();
+            
+           
         }
     }
 }
